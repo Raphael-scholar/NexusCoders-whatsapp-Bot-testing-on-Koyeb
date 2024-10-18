26c581986ae3ca81fb2c9bd8b3ad9a8a2ce02fe6
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-18 13:34:01 +0100

    Update database.js

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/nexusbot', {
+        await mongoose.connect(process.env.MONGODB_URI || '', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
