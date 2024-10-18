f8dab31de11a6ae5da8b687774096df4897ddb52
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-18 18:26:34 +0100

    Update commandHandler.js

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -1,57 +1,37 @@
 const fs = require('fs-extra');
 const path = require('path');
-const logger = require('../utils/logger');
 
-let commandCache = new Map();
+const commands = new Map();
 
-const loadCommandsFromDirectory = async (directory) => {
-    const commands = new Map();
+const loadCommands = async (dir) => {
+    const files = await fs.readdir(dir);
     
-    try {
-        const items = await fs.readdir(directory, { withFileTypes: true });
+    for (const file of files) {
+        const fullPath = path.join(dir, file);
+        const stat = await fs.stat(fullPath);
         
-        for (const item of items) {
-            const fullPath = path.join(directory, item.name);
-            
-            if (item.isDirectory()) {
-                const subCommands = await loadCommandsFromDirectory(fullPath);
-                for (const [name, command] of subCommands) {
-                    commands.set(name, command);
-                }
-            } else if (item.name.endsWith('.js')) {
-                const command = require(fullPath);
-                if (command.name && command.execute) {
-                    commands.set(command.name, command);
-                }
-            }
+        if (stat.isDirectory()) {
+            await loadCommands(fullPath);
+        } else if (file.endsWith('.js')) {
+            const command = require(fullPath);
+            commands.set(command.name, command);
         }
-    } catch (error) {
-        logger.error(`Error loading commands from ${directory}:`, error);
     }
-    
-    return commands;
 };
 
 const initializeCommands = async () => {
     try {
         const commandsPath = path.join(__dirname, '..', 'commands');
-        await fs.ensureDir(commandsPath);
-        
-        commandCache = await loadCommandsFromDirectory(commandsPath);
-        logger.info(`Loaded ${commandCache.size} commands successfully`);
-        
-        return commandCache;
+        await loadCommands(commandsPath);
+        return commands;
     } catch (error) {
-        logger.error('Failed to initialize commands:', error);
         return new Map();
     }
 };
 
-const searchCommands = async (commandName) => {
-    return commandCache.get(commandName);
-};
+const getCommands = () => commands;
 
 module.exports = {
     initializeCommands,
-    searchCommands
+    getCommands
 };
