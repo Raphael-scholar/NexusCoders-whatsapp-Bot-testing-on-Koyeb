ba862fc1b96cd5ed6d44cd90b752c4225db5465b
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-18 08:38:55 +0100

    Update commandHandler.js

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -1,27 +1,59 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
-
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const NodeCache = require('node-cache');
+const config = require('../config');
+const logger = require('../utils/logger');
+const { checkPermissions } = require('../utils/permissions');
 
 const commands = new Map();
+const cooldowns = new NodeCache({ stdTTL: 30 });
 
-for (const file of commandFiles) {
-    const command = require(path.join(commandsPath, file));
-    commands.set(command.name, command);
-}
+async function initializeCommands() {
+    const commandsPath = path.join(__dirname, '../commands');
+    const categories = await fs.readdir(commandsPath);
 
-async function handleCommand(sock, msg, command, args) {
-    if (commands.has(command)) {
-        try {
-            await commands.get(command).execute(sock, msg, args);
-        } catch (error) {
-            console.error(`Error executing command ${command}:`, error);
-            await sock.sendMessage(msg.key.remoteJid, { text: 'There was an error executing that command.' });
+    for (const category of categories) {
+        const categoryPath = path.join(commandsPath, category);
+        const commandFiles = (await fs.readdir(categoryPath)).filter(file => file.endsWith('.js'));
+
+        for (const file of commandFiles) {
+            const command = require(path.join(categoryPath, file));
+            commands.set(command.name, command);
         }
-    } else {
-        await sock.sendMessage(msg.key.remoteJid, { text: 'Unknown command. Type !help for a list of commands.' });
     }
 }
 
-module.exports = { handleCommand };
+async function executeCommand(messageInfo, commandName, args) {
+    const { sock, msg, sender, isGroup, groupMetadata, user } = messageInfo;
+    const command = commands.get(commandName);
+    
+    if (!command) return;
+
+    const cooldownKey = `${sender}-${commandName}`;
+    if (cooldowns.has(cooldownKey)) {
+        const timeLeft = cooldowns.getTtl(cooldownKey) - Date.now();
+        await sock.sendMessage(sender, {
+            text: config.messages.cooldown.replace('{time}', Math.ceil(timeLeft / 1000))
+        });
+        return;
+    }
+
+    try {
+        const permissionCheck = await checkPermissions(messageInfo, command);
+        if (!permissionCheck.granted) {
+            await sock.sendMessage(sender, { text: permissionCheck.message });
+            return;
+        }
+
+        await command.execute(messageInfo, args);
+        cooldowns.set(cooldownKey, true, command.cooldown || 3);
+    } catch (error) {
+        logger.error(`Command execution error (${commandName}):`, error);
+        await sock.sendMessage(sender, { text: config.messages.error });
+    }
+}
+
+module.exports = {
+    initializeCommands,
+    executeCommand
+};
